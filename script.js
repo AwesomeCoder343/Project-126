882b54b4c60c00d71084ebcae64a95d781d7216b
AwesomeCoder343 <73550340+AwesomeCoder343@users.noreply.github.com>
2021-03-19 17:35:57 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWristY=0;
+scoreRightWrist=0;
+scoreLeftWrist=0;
 function preload(){
    song1=loadSound("music.mp3");
    song2=loadSound("music2.mp3");
@@ -15,6 +17,18 @@ function setup(){
 }
 function draw(){
     image(video, 0,0,600,600);
+    fill('red');
+    stroke('red');
+    if(scoreLeftWrist>0.2){
+        circle(leftWristX, leftWristY, 30);
+        numberleftWristY=Number(leftWristY)
+        removedecimal=floor(numberleftWristY);
+        console.log(removedecimal);
+        volume=removedecimal/500;
+        document.getElementById("div_volume1").innerHTML="Volume = " + volume;
+        song.setVolume(volume);
+        console.log(volume);
+    }
 }
 function play(){
     console.log("HI");
@@ -38,6 +52,11 @@ function gotPoses(results){
         console.log(rightWristX);
         rightWristY=results[0].pose.rightWrist.y;
         console.log(rightWristY);
+        
+    scoreRightWrist=results[0].pose.keypoints[10].score;
+    scoreLeftWrist=results[0].pose.keypoints[9].score;
+    console.log(scoreLeftWrist);
+    console.log(scoreRightWrist);
     }
     }
     
